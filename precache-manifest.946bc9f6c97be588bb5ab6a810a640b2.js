400165fda8d3cfa977d566ca96f9ad6f2f9cfc89
Stanisław Mazowiecki <smazowiecki@g2a.com>
2020-06-25 22:15:04 +0200

    Updates

---
+++ precache-manifest.946bc9f6c97be588bb5ab6a810a640b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82986cc1222683436c553d92ed830517",
+    "url": "/MultiplicateIt/index.html"
+  },
+  {
+    "revision": "6d3b3c2bba939267dd88",
+    "url": "/MultiplicateIt/static/css/main.934e14e6.chunk.css"
+  },
+  {
+    "revision": "626834a4395c03549a5b",
+    "url": "/MultiplicateIt/static/js/2.a39b87cd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MultiplicateIt/static/js/2.a39b87cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d3b3c2bba939267dd88",
+    "url": "/MultiplicateIt/static/js/main.7c15100d.chunk.js"
+  },
+  {
+    "revision": "c6b98aebca702e741b58",
+    "url": "/MultiplicateIt/static/js/runtime-main.f3980711.js"
+  }
+]);
